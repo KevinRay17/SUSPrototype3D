3f55df8704d2932f7ecadecc09608f6dbfcc462c
Connor <cak549@nyu.edu>
2019-02-06 13:51:52 -0500

    Initial commit

---
+++ Assets/Scenes/Scripts/Description.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Description : MonoBehaviour
+{
+    public GameObject descriptorPrefab;
+
+    public Transform camera;
+
+    public Canvas canvas;
+    
+    public float yOffset;
+
+    private GameObject descriptor;
+    
+    private Text name;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        descriptor = Instantiate(descriptorPrefab);
+
+        descriptor.transform.parent = canvas.transform;
+        
+        descriptor.transform.position = new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z);
+        
+        descriptor.transform.LookAt(camera);
+
+        EditName();
+    }
+
+    private void OnMouseExit()
+    {
+        Destroy(descriptor);
+    }
+
+    void EditName()
+    {
+        name = descriptor.GetComponentInChildren<Text>();
+
+        name.text = tag;
+    }
+}
