97216c2b318b646b7ff261d6c1992cbab0f212f0
Connor <cak549@nyu.edu>
2019-02-06 14:31:26 -0500

    Added TestMaterial scene

--- Assets/Scenes/Scripts/Description.cs
+++ Assets/Scenes/Scripts/Description.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Description manager script, attached to any gameObject that needs a descriptive tag for the player.
+/// GameObjects are assigned in the inspector (temp), will be automatic eventually
+/// </summary>
 public class Description : MonoBehaviour
 {
+    // Description prefab, contains UI image and UI text objects
     public GameObject descriptorPrefab;
 
+    // Main camera in the scene
     public Transform camera;
 
     public Canvas canvas;
     
+    // Offset so the descriptor appears above the gameObject, editable in the inspector
     public float yOffset;
 
+    // The instantiated descriptor, NOT the prefab
+    // Declared private for accessibility
     private GameObject descriptor;
     
+    // The UI 
     private Text name;
     
     // Start is called before the first frame update
