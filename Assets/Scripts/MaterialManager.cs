3695bd4570a1a1129c6edb4f14bca1d40a9e8d1f
Connor <cak549@nyu.edu>
2019-02-07 16:36:46 -0500

    Material transfer is now tag-based, mats are now set according to tag.

---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+public class MaterialManager : MonoBehaviour
+{
+    public GameObject heldMaterial;
+
+    public Material defaultMat;
+
+    private Renderer heldMaterialRenderer;
+
+    private Renderer thisRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the MeshRenderer from the heldMaterial gameObject
+        heldMaterialRenderer = heldMaterial.GetComponent<MeshRenderer>();
+
+        thisRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (tag != "Blank")
+            {
+                heldMaterialRenderer.material = thisRenderer.material;
+
+                thisRenderer.material = defaultMat;
+
+                // Sets heldMaterial tag to the current object's tag
+                heldMaterial.tag = this.tag;
+
+                // Current object's tag is set to blank
+                this.tag = "Blank";
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            thisRenderer.material = heldMaterialRenderer.material;
+            
+            heldMaterialRenderer.material = defaultMat;
+
+            this.tag = heldMaterial.tag;
+
+            heldMaterial.tag = "Blank";
+        }
+    }
+}
